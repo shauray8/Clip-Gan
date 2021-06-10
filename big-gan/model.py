0858d62a436a6e5996a81c7223fd5b12b8967639
Shauray <shauray9@gmail.com>
2021-06-10 22:03:14 +0530

    structure for GAN

--- big-gan/model.py
+++ big-gan/model.py
@@ -4,6 +4,22 @@ import numpy as np
 import torch.nn.functional as F
 from torch.nn.init import kaiming_normal_, constant_
 
+class Generator(nn.Module):
+    def __init__(self):
+        super(Generator, self).__init__()
+        pass
+    
+    def forward(self):
+        pass
+
+class Discriminator(nn.Module):
+    def __init__(self):
+        super(Discriminator, self).__init__()
+        pass
+    
+    def forward(self):
+        pass
+
 
 if __name__ == "__main__":
     print("Model goes here")
